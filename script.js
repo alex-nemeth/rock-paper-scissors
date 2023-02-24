416cee80eef273c9edfdb96ee7d4418635dcb03c
alex-nemeth <116309918+alex-nemeth@users.noreply.github.com>
2023-02-24 12:33:46 +0100

    Updated script.js
    Game is now played in a best of 3 set . Script also produces alert windows with updates on the game set.

--- script.js
+++ script.js
@@ -6,47 +6,94 @@ function getComputerChoice() {
 }
 
 function getPlayerSelection() {
-    const selection = prompt("Rock, Paper or Scissors?");
-    return selection.toLowerCase();
+    let preSelection = prompt("Rock, Paper or Scissors?");
+    let selection = preSelection.toLowerCase();
+    while (true) {
+        if (
+            selection === "rock" ||
+            selection === "paper" ||
+            selection === "scissors"
+        )
+            break;
+        else {
+            alert("invalid input (type rock, paper or scissors!");
+            preSelection = prompt("Rock, Paper or Scissors?");
+            selection = preSelection.toLowerCase();
+        }
+    }
+    return selection;
 }
 
-function playRound(player, cpu) {
+function playRound() {
+    let cpu = getComputerChoice();
+    let player = getPlayerSelection();
     if (player === "rock") {
-        if (cpu === "rock")
+        if (cpu === "rock") {
             console.log(
                 `Draw! ${player} against ${cpu} is an unresolved conflict.`
             );
-        if (cpu === "paper")
+            alert(`Draw! ${player} against ${cpu} is an unresolved conflict.`);
+            return 0;
+        } else if (cpu === "paper") {
             console.log("You lose! " + cpu + " beats " + player);
-        else console.log("You win! " + player + " beats " + cpu);
+            alert("You lose! " + cpu + " beats " + player);
+            return -1;
+        } else {
+            console.log("You win! " + player + " beats " + cpu);
+            alert("You win! " + player + " beats " + cpu);
+            return 1;
+        }
     }
     if (player === "paper") {
-        if (cpu === "paper")
+        if (cpu === "paper") {
             console.log(
                 `Draw! ${player} against ${cpu} is an unresolved conflict.`
             );
-        if (cpu === "scissors")
+            alert(`Draw! ${player} against ${cpu} is an unresolved conflict.`);
+            return 0;
+        } else if (cpu === "scissors") {
             console.log("You lose! " + cpu + " beats " + player);
-        else console.log("You win! " + player + " beats " + cpu);
+            alert("You lose! " + cpu + " beats " + player);
+            return -1;
+        } else {
+            console.log("You win! " + player + " beats " + cpu);
+            alert("You win! " + player + " beat " + cpu);
+            return 1;
+        }
     }
     if (player === "scissors") {
-        if (cpu === "scissors")
+        if (cpu === "scissors") {
             console.log(
                 `Draw! ${player} against ${cpu} is an unresolved conflict.`
             );
-        if (cpu === "rock")
+            alert(`Draw! ${player} against ${cpu} is an unresolved conflict.`);
+            return 0;
+        } else if (cpu === "rock") {
             console.log("You lose! " + cpu + " beats " + player);
-        else console.log("You win! " + player + " beat " + cpu);
+            alert("You lose! " + cpu + " beats " + player);
+            return -1;
+        } else {
+            console.log("You win! " + player + " beat " + cpu);
+            alert("You win! " + player + " beat " + cpu);
+            return 1;
+        }
     }
 }
 
-const cpu = getComputerChoice();
-let player = getPlayerSelection();
-while (true) {
-    if (player === "rock" || player === "paper" || player === "scissors") break;
-    else {
-        alert("invalid input (type rock, paper or scissors!");
-        player = getPlayerSelection();
+function game() {
+    const numOfGames = 3;
+    let pWins = 0;
+    let cWins = 0;
+    let round = 0;
+    for (let i = 1; i <= numOfGames; i++) {
+        round = playRound();
+        if (round === 0) i--;
+        else if (round === 1) pWins++;
+        else cWins++;
+        alert(`Player ${pWins} - ${cWins} Computer`);
     }
+    console.log(`Game over! Player: ${pWins} - ${cWins} : Computer`);
+    console.log(`Game over! Player: ${pWins} - ${cWins} : Computer`);
 }
-playRound(player, cpu);
+
+game();
