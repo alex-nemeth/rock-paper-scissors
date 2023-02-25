b83736611b2e14d35635dab774f3a20851e10750
alex-nemeth <116309918+alex-nemeth@users.noreply.github.com>
2023-02-25 20:26:23 +0100

    basic gui v1

--- script.js
+++ script.js
@@ -1,94 +1,42 @@
 function getComputerChoice() {
     let num = Math.floor(Math.random() * 3) + 1;
-    if (num === 1) return "rock";
-    if (num === 2) return "paper";
-    if (num === 3) return "scissors";
+    if (num === 1) return "Rock";
+    if (num === 2) return "Paper";
+    if (num === 3) return "Scissors";
 }
-
-function getPlayerSelection() {
-    let preSelection = prompt("Rock, Paper or Scissors?");
-    let selection = preSelection.toLowerCase().trim();
-    let count = 0;
-    while (true) {
-        if (
-            selection === "rock" ||
-            selection === "paper" ||
-            selection === "scissors"
-        )
-            break;
-        else {
-            count++;
-            if (count === 3) {
-                alert("Clueless");
-                window.location.replace(
-                    "https://www.youtube.com/watch?v=dQw4w9WgXcQ://www.w3schools.com"
-                );
-                exit;
-            }
-            alert("invalid input (type rock, paper or scissors!");
-            preSelection = prompt("Rock, Paper or Scissors?");
-            selection = preSelection.toLowerCase().trim();
-        }
-    }
-    return selection;
-}
-
-function playRound() {
-    let cpu = getComputerChoice();
-    let player = getPlayerSelection();
-    if (player === "rock") {
-        if (cpu === "rock") {
-            console.log(
-                `Draw! ${player} against ${cpu} is an unresolved conflict.`
-            );
-            alert(`Draw! ${player} against ${cpu} is an unresolved conflict.`);
-            return 0;
-        } else if (cpu === "paper") {
-            console.log("You lose! " + cpu + " beats " + player);
-            alert("You lose! " + cpu + " beats " + player);
+function playRound(player) {
+    cpu = getComputerChoice();
+    if (player === cpu) {
+        gameText.textContent = "Draw!";
+        return 0;
+    } else if (player === "Rock") {
+        if (cpu === "Paper") {
+            gameText.textContent = "You lose! " + cpu + " beats " + player;
             return -1;
         } else {
-            console.log("You win! " + player + " beats " + cpu);
-            alert("You win! " + player + " beats " + cpu);
+            gameText.textContent = "You win! " + player + " beats " + cpu;
             return 1;
         }
     }
-    if (player === "paper") {
-        if (cpu === "paper") {
-            console.log(
-                `Draw! ${player} against ${cpu} is an unresolved conflict.`
-            );
-            alert(`Draw! ${player} against ${cpu} is an unresolved conflict.`);
-            return 0;
-        } else if (cpu === "scissors") {
-            console.log("You lose! " + cpu + " beats " + player);
-            alert("You lose! " + cpu + " beats " + player);
+    if (player === "Paper") {
+        if (cpu === "Scissors") {
+            gameText.textContent = "You lose! " + cpu + " beats " + player;
             return -1;
         } else {
-            console.log("You win! " + player + " beats " + cpu);
-            alert("You win! " + player + " beat " + cpu);
+            gameText.textContent = "You win! " + player + " beats " + cpu;
             return 1;
         }
     }
-    if (player === "scissors") {
-        if (cpu === "scissors") {
-            console.log(
-                `Draw! ${player} against ${cpu} is an unresolved conflict.`
-            );
-            alert(`Draw! ${player} against ${cpu} is an unresolved conflict.`);
-            return 0;
-        } else if (cpu === "rock") {
-            console.log("You lose! " + cpu + " beats " + player);
-            alert("You lose! " + cpu + " beats " + player);
+    if (player === "Scissors") {
+        if (cpu === "Rock") {
+            gameText.textContent = "You lose! " + cpu + " beats " + player;
             return -1;
         } else {
-            console.log("You win! " + player + " beat " + cpu);
-            alert("You win! " + player + " beat " + cpu);
+            gameText.textContent = "You win! " + player + " beat " + cpu;
             return 1;
         }
     }
 }
-
 function game(numOfGames) {
     let pWins = 0;
     let cWins = 0;
@@ -98,29 +46,34 @@ function game(numOfGames) {
         if (round === 0) i--;
         else if (round === 1) pWins++;
         else cWins++;
-        alert(`Player ${pWins} - ${cWins} Computer`);
+        gameText.textContent = `Player ${pWins} - ${cWins} Computer`;
     }
-    console.log(`Game over! Player: ${pWins} - ${cWins} : Computer`);
-    alert(`Game over! \nPlayer: ${pWins} - ${cWins} : Computer`);
+    gameText.textContent = `Game over! \nPlayer: ${pWins} - ${cWins} : Computer`;
 }
 
-function gameSet() {
-    let set = 0;
-    let count = 0;
-    set = prompt("How many wins do you want to play for?\nEnter a number:");
-    while (isNaN(set)) {
-        count++;
-        if (count === 3) {
-            alert("Clueless");
-            window.location.replace(
-                "https://www.youtube.com/watch?v=dQw4w9WgXcQ://www.w3schools.com"
-            );
-            exit;
+let scoreText = document.querySelector(".score");
+let gameText = document.querySelector(".top");
+
+let playerSelection;
+let computerSelection;
+let playerScore = 0;
+let cpuScore = 0;
+let result;
+
+const buttons = document.querySelectorAll(".selection");
+
+buttons.forEach((button) =>
+    button.addEventListener("click", () => {
+        playerSelection = button.textContent;
+        result = playRound(playerSelection);
+        if (result === 1) {
+            playerScore++;
         }
-        set = prompt(
-            `Invalid input!\nHow many wins do you want to play for?\nEnter a number:`
-        );
-    }
-    return set;
-}
-game(gameSet());
+        if (result === -1) {
+            cpuScore++;
+        }
+        scoreText.textContent = `${playerScore} - ${cpuScore}`;
+    })
+);
+
+//game(gameSet());
